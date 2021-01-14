e279bfc5737d193bbc9bb311c5da88cf65ff91a0
NJohnston94 <nathanj.94@hotmail.com>
2021-01-14 16:38:15 +0000

    Headers and their values can be tested with exceptions thrown for null values, closing issue15

--- src/test/java/com/sparta/nj/swapitestframework/connection/ConnectionManager.java
+++ src/test/java/com/sparta/nj/swapitestframework/connection/ConnectionManager.java
@@ -1,11 +1,14 @@
 package com.sparta.nj.swapitestframework.connection;
 
 import com.sparta.nj.swapitestframework.exceptions.BadStatusCodeException;
+import com.sparta.nj.swapitestframework.exceptions.HeaderNotFoundException;
 import io.restassured.RestAssured;
 import io.restassured.http.Header;
 import io.restassured.response.Response;
 
 import java.util.List;
+
+import static io.restassured.RestAssured.given;
 
 public class ConnectionManager {
     public ConnectionManager() {
@@ -41,6 +44,17 @@ public class ConnectionManager {
         return RestAssured.get(url).getStatusCode();
     }
 
+    public static List<Header> getResourceHeaders(String url) {
+        Response response = connectToResource(url);
+        List<Header> resourceHeaders = response.getHeaders().asList();
+        System.out.println("Response returned with Headers::");
+        for(Header header:resourceHeaders) {
+            System.out.println("   " + header);
+        }
+
+        return resourceHeaders;
+    }
+
     public static List<Header> getResourceHeaders(Response response) {
         List<Header> resourceHeaders = response.getHeaders().asList();
         System.out.println("Response returned with Headers::");
@@ -49,5 +63,61 @@ public class ConnectionManager {
         }
 
         return resourceHeaders;
+    }
+
+    public static String getHeaderValue(String headerName, String url) throws HeaderNotFoundException {
+        String headerValue = null;
+        List<Header> resourceHeaders = getResourceHeaders(url);
+        for(Header header:resourceHeaders) {
+            if(header.toString().contains(headerName)) {
+                headerValue = given().response().when().get(url).getHeader(headerName);
+            }
+        }  
+        
+        if(headerValue == null) {
+            throw new HeaderNotFoundException();
+        } else {
+            return headerValue;
+        }
+    }
+
+    public static String getHeaderValue(String headerName, Response response) throws HeaderNotFoundException {
+        String headerValue = null;
+        List<Header> resourceHeaders = getResourceHeaders(response);
+        String url = response.then().extract().path("url").toString();
+        for(Header header:resourceHeaders) {
+            if(header.toString().contains(headerName)) {
+                headerValue = given().response().when().get(url).getHeader(headerName);
+            }
+        }
+
+        if(headerValue == null) {
+            throw new HeaderNotFoundException();
+        } else {
+            return headerValue;
+        }
+    }
+
+    public static boolean testResponseHeaders(String headerName, String headerValue, String url) {
+        List<Header> resourceHeaders = getResourceHeaders(url);
+        for(Header header:resourceHeaders) {
+            if(header.toString().contains(headerName)) {
+                System.out.println(headerName + " = " + headerValue + " is TRUE");
+                return given().response().when().get(url).getHeader(headerName).equals(headerValue);
+            }
+        }
+        System.out.println(headerName + " = " + headerValue + " is FALSE");
+        return false;
+    }
+
+    public static boolean testResponseHeaders(String headerName, String headerValue, Response response) {
+        List<Header> resourceHeaders = getResourceHeaders(response);
+        String url = response.then().extract().path("url").toString();
+        for(Header header:resourceHeaders) {
+            if(header.toString().contains(headerName)) {
+                return given().response().when().get(url).getHeader(headerName).equals(headerValue);
+            }
+        }
+        return false;
     }
 }
